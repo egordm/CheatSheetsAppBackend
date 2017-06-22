38598565a1e461773c3ef6444bc4f0bb49d29789
EgorDm <egordmitriev2@gmail.com>
2017-06-22 22:36:21 +0200

    Added mass add method

--- admin/Controllers/MainController.php
+++ admin/Controllers/MainController.php
@@ -2,6 +2,14 @@
 namespace Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Serializers\FractalDataSerializer;
+use App\Models\Transformers\CategoryTransformer;
+use Exception;
+use Illuminate\Http\Request;
+use JsonMapper;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
 
 /**
  * Created by PhpStorm.
@@ -13,5 +21,31 @@ class MainController extends Controller
 {
     public function index() {
         return 'Hello world!';
+    }
+
+    public function massAdd(Request $request) {
+        ini_set('max_execution_time', 0);
+        $content_my = $request->getContent();
+
+        $mapper = new JsonMapper();
+        $mapper->bIgnoreVisibility = true;
+        $contact = $mapper->mapArray(json_decode($content_my), [], Category::class);
+
+        \DB::beginTransaction();
+        try {
+            foreach ($contact as $con) {
+                $con->push();
+            }
+        } catch (Exception $e) {
+            \DB::rollback();
+        } finally {
+            \DB::commit();
+        }
+
+        $resource = new Collection($contact, new CategoryTransformer());
+
+        $manager = new Manager();
+        $manager->setSerializer(new FractalDataSerializer());
+        return $manager->createData($resource)->toArray();
     }
 }
