fae19d51698c4cb352e3f61ecdaac71c0da9dcb8
EgorDm <egordmitriev2@gmail.com>
2017-06-22 19:37:58 +0200

    Fixed cheat sheet not found

--- api/Controllers/MainController.php
+++ api/Controllers/MainController.php
@@ -26,14 +26,6 @@ class MainController extends Controller
         return JsonResponse::create($this->getManager()->createData($data)->toArray());
     }
 
-    public function cheatsheet($id)
-    {
-        $raw_data = CheatSheet::with('tags', 'cheat_groups', 'cheat_groups.tags', 'cheat_groups.notes',
-            'cheat_groups.cheats', 'cheat_groups.cheats.cheat_contents', 'cheat_groups.tags')->find($id);
-        $data = CheatSheet::transform($raw_data);
-        return JsonResponse::create($this->getManager('cheat_groups')->createData($data)->toArray());
-    }
-
     /**
      * @param string|array|null $include
      * @return Manager
@@ -44,6 +36,15 @@ class MainController extends Controller
         $manager->setSerializer(new FractalDataSerializer());
         if ($include != null) $manager->parseIncludes($include);
         return $manager;
+    }
+
+    public function cheatsheet($id)
+    {
+        $raw_data = CheatSheet::with('tags', 'cheat_groups', 'cheat_groups.tags', 'cheat_groups.notes',
+            'cheat_groups.cheats', 'cheat_groups.cheats.cheat_contents', 'cheat_groups.tags')->find($id);
+        if (empty($raw_data)) return JsonResponse::create(['message' => 'Cheat sheet not found!'], 404);
+        $data = CheatSheet::transform($raw_data);
+        return JsonResponse::create($this->getManager('cheat_groups')->createData($data)->toArray());
     }
 
 
